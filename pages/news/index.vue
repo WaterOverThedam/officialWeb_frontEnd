477899e8a2d920d087a92d0e04062b2b84fe4bdf
Zhang Shuo <zs@xx.com>
2018-04-17 15:40:17 +0800

    3

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -77,12 +77,8 @@
             <MyMedia></MyMedia>
         </main>
         <GoTop></GoTop>
-<<<<<<< HEAD
-        <MyFooter></MyFooter>
-=======
         <!-- <MyFooter :bgColor="bgColor_cur"></MyFooter> -->
         <MyFooter ></MyFooter>
->>>>>>> b30316f05005623314651ecc45c9e7bc520f4d41
 
 	</div>
 	
